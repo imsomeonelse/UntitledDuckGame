461fc5d8bbcec130a7dcf7d8ad6d47205853ed42
Andres Sedano <andysedano@gmail.com>
2021-01-28 12:56:54 -0800

    Switch cameras
    Swtich cameras with q and e

---
+++ Assets/Scripts/CameraSwitcher.cs
@@ -0,0 +1,57 @@
+using Cinemachine;
+using UnityEngine;
+
+public class CameraSwitcher : MonoBehaviour
+{
+    private int _CurrentCam = 0;
+
+    [SerializeField]
+    private CinemachineVirtualCamera[] _Cameras;
+
+    private void Start()
+    {
+        // Get all child cameras
+        _Cameras = GetComponentsInChildren<CinemachineVirtualCamera>();
+
+        // disable all cameras
+        foreach (var cam in _Cameras)
+        {
+            cam.gameObject.SetActive(false);
+        }
+
+        // Enable the first one
+        _Cameras[0].gameObject.SetActive(true);
+
+        Debug.Log(-1 % 3);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            LeftCam();
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+
+            RightCam();
+        }
+    }
+
+    public void RightCam()
+    {
+        _Cameras[_CurrentCam].gameObject.SetActive(false);
+        _CurrentCam = (_CurrentCam - 1 + 4) % 4;
+        _Cameras[_CurrentCam].gameObject.SetActive(true);
+    }
+
+    public void LeftCam()
+    {
+        _Cameras[_CurrentCam].gameObject.SetActive(false);
+        _CurrentCam = (_CurrentCam + 1) % 4;
+        _Cameras[_CurrentCam].gameObject.SetActive(true);
+    }
+}
--- Assets/Scripts/DuckMovement.cs
+++ Assets/Scripts/DuckMovement.cs
@@ -2,19 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DuckMovement: MonoBehaviour
+public class DuckMovement : MonoBehaviour
 {
     public float speed = 1.0f;
+
+    public Camera MainCam;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        MainCam = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W)) {
+        if (Input.GetKey(KeyCode.W))
+        {
             this.transform.position += Vector3.forward * speed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.S))
