b9cbbee476c6b3fc2cab1c357547546b795b2570
CantuckyLV <danielgc0794@gmail.com>
2021-01-28 19:30:28 -0800

    GameDirector WIP

--- Assets/Scripts/DucklingAI.cs
+++ Assets/Scripts/DucklingAI.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Duckling : MonoBehaviour
+public class DucklingAI : MonoBehaviour
 {
     
     public GameObject targetDuckling;
---
+++ Assets/Scripts/GameDirector.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameDirector : MonoBehaviour
+{
+    private int brothersToWin = 4;
+    private int currentBrothers = 0;
+    private GameObject player;
+    private GameObject sewerExit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        sewerExit = GameObject.Find("SewerExit");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        currentBrothers = player.GetComponent<MoveInteractor>().ducklingStack.Count;
+        if (currentBrothers == brothersToWin) {
+            Destroy(sewerExit);
+        }
+    }
+}
